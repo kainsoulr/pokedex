1ed85bb77a9f094b9c2f4c5040b088faf6fe3a72
kainsoulr <kainsoulreaver999@gmail.com>
2023-10-09 09:09:10 +0200

    pokemon page

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+export const PokemonList = () => {
+    return (
+        <>
+            <div className="card-list-pokemon container">
+
+            </div>
+        </>
+    )
+}
